16305e7a14a92d6bce0041f8e1904c200d76152d
Babosa <mysunpany@gmail.com>
2015-10-24 14:44:38 +0800

    remove EasyDarwin Project

--- EasyDarwin/WinNTSupport/html/english/language.js
+++
@@ -1,7 +0,0 @@
-﻿var str_user = "Name";
-var str_pwd = "Password";
-var str_language="Language"
-var str_login="Login"
-var str_login_warn="The name or password is incorrect"
-var str_pwd_warn="The password can not be empty!"
-var str_user_warn="The user can not be empty!"
